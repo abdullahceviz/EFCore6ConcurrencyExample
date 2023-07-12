73032e2f65b4a7d5343e491bedd5b9a78a8a7337
Abdullah Ceviz <abdullah.ceviz.resmi@gmail.com>
2023-07-12 17:16:48 +0300

    project init

---
+++ ConcurrencyExample/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ConcurrencyExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
